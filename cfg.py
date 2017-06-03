9555584ae68bbacb3116102ccf804f7f8471ef07
rrunge <RRunge@trefoil3.Dlink>
2017-06-03 18:17:18 +0300

    init test

---
+++ cfg.py
@@ -0,0 +1,17 @@
+# cfg.py
+# Contains the configurations for the bot
+
+#~ HOST = "irc.twitch.tv"
+HOST = "irc.chat.twitch.tv"
+PORT = 6667
+#bot's name
+NICK = ""
+#pass is token from https://twitchapps.com/tmi/
+PASS = ""
+CHAN = ""
+# messages per second
+RATE = (20/30)
+
+
+oplist = {}
+stronglist = []
